85f74431bee0125ef000a4906b803520278d61a4
Cooper <eliteswimmer42@gmail.com>
2020-08-24 12:22:35 -0500

    testing some console.logs

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var APIKey = "ade2bb7e46d866c6271ae23428c893bc";
 
 $(document).ready(function() {
+  console.log(ready);
   $("#citySearch").on("click", function() {
     var cityInput = $("#cityInput").val();
+    console.log("click");
     // stringify?
     $("#cityInput").val("");
-    console.log(cityWeather(cityInput));
+    // console.log(cityWeather(cityInput));
   });
 
 
@@ -16,13 +18,14 @@ $(document).ready(function() {
   function cityWeather(city) {
     // API AND AJAX
     var city = $("#cityInput").val();
+    console.log(city);
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us" + APIKey;
     $.ajax({
       type: "GET",
       url: queryURL,
       dataType: "json",
       success: function(data) {
-
+        console.log(data);
         $("#cityForm").empty();
 
 
@@ -31,17 +34,18 @@ $(document).ready(function() {
         var cityTemp = $(".cityTemp").html("#cityDiv").text("Temperature is " + data.main.temp + "F");
         var cityWind = $("#cityWind").html("#cityDiv").text("Wind speed is " + data.wind.speed + "mph");
         var cityHumid = $("#cityHum").html("#cityDiv").text("Humidity is " + data.main.humidity + "%");
-        var weatherIcon = $("<img>").attr("src", "http://openweatherapp.org/img/w/" + data.weather[0].icon + ".png");
+        var weatherIcon = $(".weatherIcon").attr("src", "http://openweatherapp.org/img/w/" + data.weather[0].icon + ".png");        
+        
+        // console.log(cityName);
+        // console.log(cityHumid);
+        // console.log(cityWind);
+        // console.log(cityTemp);
         // calling the above variables to appending to our card
         cityName.append(weatherIcon);
         cardBody.append(cityName, cityTemp, cityWind, cityHumid);
         card.append(cardBody);
         $("#cityForm").append(card);
 
-        console.log(cityName);
-        console.log(cityHumid);
-        console.log(cityWind);
-        console.log(cityTemp);
         // cityForecast();
         // cityUVIndex();
       }
