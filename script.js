89daff80b32fe7ff3f0d4f4116125fb109eba723
Cooper <eliteswimmer42@gmail.com>
2020-08-25 17:16:40 -0500

    attempting to fix github live pages

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 var APIKey = "&appid=ade2bb7e46d866c6271ae23428c893bc";
 
 $(document).ready(function () {
-
   $("#citySearch").on("click", function () {
     event.preventDefault();
-
     var cityInput = $("#cityInput").val().trim();
     $("#cityInput").val("");
 
-
     function cityWeather(city) {
-
-      var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us" + APIKey;
+      var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us" + APIKey;
       $.ajax({
         type: "GET",
         url: queryURL,
         dataType: "json",
         success: function (data) {
-          console.log(data);
+          
           $("#cityInput").empty();
 
           // variables to store cityName, cityTemp, cityHumid, cityWind, cityUV
@@ -26,23 +22,18 @@ $(document).ready(function () {
           var cityWind = $(".cityWind").html("#cityDiv").text("Wind speed: " + data.wind.speed + "mph");
           var cityHumid = $(".cityHum").html("#cityDiv").text("Humidity: " + data.main.humidity + "%");
           // var cityUV = $(".cityUV").html("#cityDiv").text("UV Index: " + data.)
-          var weatherIcon = $(".weatherIcon").attr("src", "http://openweatherapp.org/img/w/" + data.weather[0].icon + ".png");
+          var weatherIcon = $(".weatherIcon").attr("src", "https://openweatherapp.org/img/w/" + data.weather[0].icon + ".png");
           // console.log(data.coord.lat, data.coord.lon);
           cityUVIndex(data.coord.lat, data.coord.lon);
         }
       })
     }
     cityWeather(cityInput);
-    // cityUVIndex(lat, lon);
-
   });
-
-  // function cachedHistory() {
-  // }
 
 
   function cityForecast(city) {
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + APIKey;
     $.ajax({
       type: "GET",
       url: queryURL,
@@ -52,12 +43,14 @@ $(document).ready(function () {
       }
     });
   }
+  cityForecast(city);
 });
+ 
 
 function cityUVIndex(lat, lon) {
   $.ajax({
     type: "GET",
-    url: `http://api.openweathermap.org/data/2.5/uvi/forecast?lat=${lat}&lon=${lon}${APIKey}`,
+    url: `https://api.openweathermap.org/data/2.5/uvi/forecast?lat=${lat}&lon=${lon}${APIKey}`,
     dataType: "json",
     success: function (data) {
       var uv = data[0].value;
