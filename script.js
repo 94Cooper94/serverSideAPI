b9eeb1e58d6a14a99add5fa55730db80b630c96b
Cooper <eliteswimmer42@gmail.com>
2020-08-27 18:42:34 -0500

    added some styling, comments, and formatted some code

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+// variables for the APIKey to easily call in functions
+// and localstorage of city search to call
 var APIKey = "&appid=ade2bb7e46d866c6271ae23428c893bc";
 var cityHistory = JSON.parse(localStorage.getItem("city")) || []
 
 
+// loads all the functions on the page upon startup
 $(document).ready(function () {
+  // click function
   $("#citySearch").on("click", function () {
     event.preventDefault();
+    // fetches city input element and user-input value
     var cityInput = $("#cityInput").val().trim();
 
-    // i will verify that cityHistory doesnt include cityIn
+    // i will verify that cityHistory doesnt include city in it
     if (cityHistory.indexOf(cityInput) === -1) {
       cityHistory.push(cityInput)
       window.localStorage.setItem("city", JSON.stringify(cityHistory));
@@ -19,6 +24,7 @@ $(document).ready(function () {
   });
 
 
+  // function that searches for city, pulling empty html elements and giving value
   function cityWeather(city) {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us" + APIKey;
     $.ajax({
@@ -41,6 +47,7 @@ $(document).ready(function () {
   }
 
 
+  // pulls the stored city input value and prepends as button below search bar
   function cachedCities() {
     cityHistory = JSON.parse(localStorage.getItem("city")) || []
     for (var i = 0; i < cityHistory.length; i++) {
@@ -48,8 +55,8 @@ $(document).ready(function () {
       cityList.text(cityHistory[i])
       cityList.addClass("list-group-item p-3 cityBtns")
       $("#searchDiv").prepend(cityList)
-
     }
+    // the onclick function that applies to the buttons prepended and runs the cityWeather function
     $(".cityBtns").on("click", function () {
       var city = $(this).text();
       cityWeather(city)
@@ -60,6 +67,7 @@ $(document).ready(function () {
 });
 
 
+// appends 5 cards each containing weather data for the next 5 days
 function cityForecast(city) {
   var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + APIKey;
   $.ajax({
@@ -74,19 +82,24 @@ function cityForecast(city) {
 }
 
 
+// separate function that reads the latitude/longitude of the searched-for city
 function cityUVIndex(lat, lon) {
   $.ajax({
     type: "GET",
     url: `https://api.openweathermap.org/data/2.5/uvi/forecast?lat=${lat}&lon=${lon}${APIKey}`,
     dataType: "json",
     success: function (data) {
+      // object containing UV value
       var uv = data[0].value;
       var cityUV = $(".cityUV").text("UV Index: " + uv);
-      // uv = 2;
+
+      // to avoid stacking separate individual buttons
       $(".cityUV").removeClass("btn-success");
       $(".cityUV").removeClass("btn-warning");
       $(".cityUV").removeClass("btn-danger");
       $(".cityUV").removeClass("btn-info");
+
+      // if statement to assign button element to UVI depending on value
       if (uv <= 3) {
         cityUV.addClass("btn-success");
         $("#cityDiv").append(cityUV.append(cityUV));
